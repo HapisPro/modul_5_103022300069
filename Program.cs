514bbe588359e10a8299eef8e666ad647dd9fa96
HapisPro <alkausarhapis@student.telkomuniversity.ac.id>
2025-03-19 07:07:29 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.ComponentModel.Design;
+
+class PemrosesData
+{
+    public T DapatkanNilaiTerbesar<T>(T digit1, T digit2, T digit3)
+    {
+        dynamic max = (dynamic) digit1;
+
+        if (max < digit2)
+        {
+            max = digit2;
+            if (max < digit3)
+            {
+                max = digit3;
+            }
+        } else if (max < digit3)
+        {
+            max = digit3;
+            if(max < digit2)
+            {
+                max = digit2;
+            }
+        }
+
+        return max;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Test");
+        PemrosesData p = new PemrosesData();
+        long nilaiTerbesar = p.DapatkanNilaiTerbesar(30, 00, 69);
+        Console.WriteLine("Nilai terbesar adalah : " + nilaiTerbesar);
+
     }
     
 }
